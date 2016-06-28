1371ade795ec5a18121fab246741984bc8126084
panpan040828 <875371217@qq.com>
2016-06-28 16:55:48 +0800

    使用面向对象的思想改写了tab

--- js/common/common_tab.js
+++ js/common/common_tab.js
@@ -3,72 +3,73 @@
  * [setTab 实现tab页切换]
  * @param {[Object]} config [传入的参数是一个hash对象]
  */
-function setTab(config) {
-	console.log(config);
-	var root = config.root;
-	var curClass = config.currentClass;
+var SetTab = function(config) {
+	this._root = config.root;
+	this._curClass = config.currentClass;
 	var trigger = config.trigger || "click";
-	var handler = config.handler;
+	this._handler = config.handler;
 	var autoPlay = config.autoPlay;
 	var playTime = config.playTime || 3000;
-	console.log(curClass);
-	var tabMenus = GLOBAL.Dom.getElementsByClassName("J_tab-menu"),
-	tabContents  = GLOBAL.Dom.getElementsByClassName("J_tab-con");//这里使用"J_tab-menu"这种形式作为className是有用途的，表明这个class与js有关
-	var curIndex = 0;
+	this._tabMenus = GLOBAL.Dom.getElementsByClassName("J_tab-menu"),
+	this._tabContents  = GLOBAL.Dom.getElementsByClassName("J_tab-con");//这里使用"J_tab-menu"这种形式作为className是有用途的，表明这个class与js有关
+	this.curIndex = 0;
 
-	/**
-	 * [showItem 显示第n个tab页]
-	 * @param  {number} n 
-	 */
-	function showItem(n) {
-		for(var i = 0; i < tabContents.length; i++) {
-			tabContents[i].style.display = "none";
-		}
-		tabContents[n].style.display = "block";//对应的tab内容显示
-		console.log(curClass);
-		if(curClass) {
-
-			var curMenu = GLOBAL.Dom.getElementsByClassName(curClass,root)[0];
-			if(curMenu) {
-				console.log("1");
-				console.log(curMenu.className);
-				GLOBAL.Dom.removeClass(curMenu,curClass);
-				console.log(curMenu.className);
-			}
-			GLOBAL.Dom.addClass(tabMenus[n],curClass);//对应的tab菜单显示
-		}
-
-		if(handler) {
-			handler(n);//回调函数
-		}
-	}
-
-	/**
-	 * [autoHandler 自动播放功能]
-	 */
-	function autoHandler() {
-		curIndex++;
-		if(curIndex >= tabMenus.length) {
-			curIndex = 0;
-		}
-		showItem(curIndex);
-	}
+	var that = this;
 
 	if(autoPlay) {
-		setInterval(autoHandler,playTime);
+		setInterval(function() { that.autoHandler() },playTime);
 	}
 
-	for(var i = 0; i < tabMenus.length; i++) {
+	for(var i = 0; i < this._tabMenus.length; i++) {
 		(function(_i) {
-			GLOBAL.Eve.addEvent(tabMenus[_i],"click",function() {
-				showItem(_i);
+			GLOBAL.Eve.addEvent(that._tabMenus[_i],"click",function() {
+				that.showItem(_i);
 			});
 		})(i);
 	}
 }
 
+SetTab.prototype = {
+	constructor: SetTab,
+
+	/**
+	 * [showItem 显示第n个tab页]
+	 * @param  {number} n 
+	 */
+	showItem: function(n) {
+		for(var i = 0; i < this._tabContents.length; i++) {
+			this._tabContents[i].style.display = "none";
+		}
+		this._tabContents[n].style.display = "block";//对应的tab内容显示
+
+		if(this._curClass) {
+
+			var curMenu = GLOBAL.Dom.getElementsByClassName(this._curClass,this._root)[0];
+			if(curMenu) {
+				GLOBAL.Dom.removeClass(curMenu,this._curClass);
+			}
+			GLOBAL.Dom.addClass(this._tabMenus[n],this._curClass);//对应的tab菜单显示
+		}
+
+		if(this._handler) {
+			this._handler(n);//回调函数
+		}
+	},
+
+	/**
+	 * [autoHandler 自动播放功能]
+	 */
+	autoHandler: function() {
+		this.curIndex++;
+		if(this.curIndex >= this.tabMenus.length) {
+			this.curIndex = 0;
+		}
+		showItem(this.curIndex);
+	}
+}
+
 var tab = GLOBAL.Dom.$("tab");
-setTab({
+new SetTab({
 	root: tab, 
 	currentClass: "tab-curMenu",
 	trigger: "click",
