a9c187d8ad29ebb67374877933b274c8cfddad88
panpan040828 <875371217@qq.com>
2016-07-03 21:42:42 +0800

    完成侧边工具栏的demo，添加一些常用的css,js

---
+++ js/common/common_tab.js
@@ -0,0 +1,79 @@
+/*common_tab.js*/
+/**
+ * [setTab 实现tab页切换]
+ * @param {[Object]} config [传入的参数是一个hash对象]
+ */
+var SetTab = function(config) {
+	this._root = config.root;
+	this._curClass = config.currentClass;
+	var trigger = config.trigger || "click";
+	this._handler = config.handler;
+	var autoPlay = config.autoPlay;
+	var playTime = config.playTime || 3000;
+	this._tabMenus = GLOBAL.Dom.getElementsByClassName("J_tab-menu"),
+	this._tabContents  = GLOBAL.Dom.getElementsByClassName("J_tab-con");//这里使用"J_tab-menu"这种形式作为className是有用途的，表明这个class与js有关
+	this.curIndex = 0;
+
+	var that = this;
+
+	if(autoPlay) {
+		setInterval(function() { that.autoHandler() },playTime);
+	}
+
+	for(var i = 0; i < this._tabMenus.length; i++) {
+		(function(_i) {
+			GLOBAL.Eve.addEvent(that._tabMenus[_i],"click",function() {
+				that.showItem(_i);
+			});
+		})(i);
+	}
+}
+
+SetTab.prototype = {
+	constructor: SetTab,
+
+	/**
+	 * [showItem 显示第n个tab页]
+	 * @param  {number} n 
+	 */
+	showItem: function(n) {
+		for(var i = 0; i < this._tabContents.length; i++) {
+			this._tabContents[i].style.display = "none";
+		}
+		this._tabContents[n].style.display = "block";//对应的tab内容显示
+
+		if(this._curClass) {
+
+			var curMenu = GLOBAL.Dom.getElementsByClassName(this._curClass,this._root)[0];
+			if(curMenu) {
+				GLOBAL.Dom.removeClass(curMenu,this._curClass);
+			}
+			GLOBAL.Dom.addClass(this._tabMenus[n],this._curClass);//对应的tab菜单显示
+		}
+
+		if(this._handler) {
+			this._handler(n);//回调函数
+		}
+	},
+
+	/**
+	 * [autoHandler 自动播放功能]
+	 */
+	autoHandler: function() {
+		this.curIndex++;
+		if(this.curIndex >= this.tabMenus.length) {
+			this.curIndex = 0;
+		}
+		showItem(this.curIndex);
+	}
+}
+
+var tab = GLOBAL.Dom.$("tab");
+new SetTab({
+	root: tab, 
+	currentClass: "tab-curMenu",
+	trigger: "click",
+	handler: function(index) {
+		alert("您点击的是第" + (index + 1) + "个标签");
+	}
+});
