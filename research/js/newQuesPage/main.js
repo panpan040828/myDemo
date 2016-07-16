40bfa849feb9b95c000a5002f96b37b3658a9dcc
panpan040828 <875371217@qq.com>
2016-07-16 21:43:36 +0800

    写了一些添加问题的页面

--- research/js/newQuesPage/main.js
+++ research/js/newQuesPage/main.js
@@ -1,4 +1,4 @@
-requirejs(["../base/base","../date3/date3"], function(G,calendar) {
+requirejs(["../base/base","../date3/date3","newQuestion"], function(G,calendar,newQuestion) {
 	var wrap = document.getElementById("dateWrap");
 	console.log(wrap);
 	new calendar.DatePicker({
@@ -7,5 +7,10 @@ requirejs(["../base/base","../date3/date3"], function(G,calendar) {
 		endY: 2049,
 		min: 3,
 		max: 15
+	});
+
+	var quesBody = G.Dom.$("newQues");
+	new newQuestion({
+		wrap: quesBody
 	});
 });
